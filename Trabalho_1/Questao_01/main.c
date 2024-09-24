139cbb4d55122dcefc49d7d62a43a61ade8f0a5b
Vinicius Nunes <viniciusnunes02612@gmail.com>
2024-09-24 09:47:31 -0300

    alterando main para inserir disciplina no curso

--- Trabalho_1/Questao_01/main.c
+++ Trabalho_1/Questao_01/main.c
@@ -8,14 +8,17 @@ int main(){
     int opcao, status;
     //criar menu de opções para o usuário
     Lista_alunos *lst_aluno;
+    Arvore_disciplina *arvore_disciplina;
     Arvore_curso *arvore_curso;
+
     arvore_curso = cria_arvore_curso();
+    arvore_disciplina = cria_arvore_disciplina();
     lst_aluno = cria_lista_alunos();
 
     do{
         printf("1 - Inserir curso\n");
         printf("2 - Inserir alunos\n");
-        printf("3 - Sair\n");
+        printf("3 - Inserir Disciplina em curso\n");
         printf("Digite a opcao desejada: ");
         scanf("%d", &opcao);
         switch(opcao){
@@ -42,10 +45,28 @@ int main(){
                 imprimir_lista_alunos(lst_aluno);
                 break;
             case 3:
-                //sair
+                Dado_disciplina disciplina;
+                disciplina = lerDadosDisciplina();
+                printf("Curso disponiveis\n");
+                imprime_arvore_cursos(arvore_curso);
+                int codigo_curso;
+                printf("Digite o codigo do curso: ");
+                scanf("%d", &codigo_curso);
+                status = adicionar_disciplina_curso(arvore_curso, disciplina, codigo_curso);
+                if(status == 1){
+                    printf("Disciplina inserida com sucesso\n");
+                }else{
+                    printf("Erro ao inserir disciplina,\n verifque se existe curso cadastrado com esse código\n");
+                }
                 break;
             default:
                 printf("Opcao invalida\n");
         }
     }while(opcao != 0);
+
+    imprime_arvore_cursos(arvore_curso);
+    imprimir_lista_alunos(lst_aluno);
+
+
+    return 0;
 }
