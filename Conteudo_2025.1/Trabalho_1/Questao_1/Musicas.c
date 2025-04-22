d3ac63a10886babce85a1597b700e367a8a7a9d8
Vincius Nunes <viniciusnunes02612@gmail.com>
2025-04-21 22:18:07 -0300

    Feat:criando remoção de musicas

--- Conteudo_2025.1/Trabalho_1/Questao_1/Musicas.c
+++ Conteudo_2025.1/Trabalho_1/Questao_1/Musicas.c
@@ -78,6 +78,91 @@ int insere_musica_album(Arvore_Album **raiz_album, Dado_Musica dado_musica){
 
 }
 
+//teste buscar menor usando recursão
+Arvore_Musica *buscar_menor_musica_direita(Arvore_Musica *raiz){
+    Arvore_Musica *menor_direita;
+    menor_direita = NULL;
+    if(raiz != NULL){
+        if(raiz->esq == NULL){
+            menor_direita = raiz;
+        }else{
+            menor_direita = buscar_menor_musica_direita(raiz->esq);
+        }
+    }
+    return menor_direita;
+}
+
+//buscar menor usando iteração
+Arvore_Musica *buscar_menor_musica_direita_itera(Arvore_Musica *raiz){
+    Arvore_Musica *menor_direita;
+    menor_direita = NULL;
+    if(raiz != NULL){
+        menor_direita = raiz;
+        while(menor_direita->esq != NULL){
+            menor_direita = menor_direita->esq;
+        }
+    }
+    return menor_direita;
+}
+
+int ehFolha_Musica(Arvore_Musica *raiz){
+    int eh_folha;
+    if(raiz->esq == NULL && raiz->dir == NULL){
+        eh_folha = 1;
+    }else{
+        eh_folha = 0;
+    }
+    return eh_folha;
+}
+
+Arvore_Musica *pega_um_filho_Musica(Arvore_Musica *raiz){
+    Arvore_Musica *auxiliar_filho;
+    auxiliar_filho = NULL;
+
+    if(raiz->esq != NULL){
+        auxiliar_filho = raiz->esq;
+    }else if(raiz->dir != NULL){
+        auxiliar_filho = raiz->dir;
+    }
+    return auxiliar_filho;
+}
+
+int remover_musica(Arvore_Musica **raiz, char nome[50]){
+    int removeu;
+    Arvore_Musica *musica_auxiliar, *filho_escolhido;
+    filho_escolhido = NULL;
+    musica_auxiliar = NULL;
+    if(*raiz != NULL) {
+        if(strcmp(nome, (*raiz)->dado.titulo) == 0) {
+            if(ehFolha_Musica(*raiz)){
+                musica_auxiliar = *raiz;
+                *raiz = NULL;
+                free(musica_auxiliar);
+            }else if((*raiz)->esq != NULL || (*raiz)->dir != NULL){
+                filho_escolhido = pega_um_filho_Musica(*raiz);
+                musica_auxiliar = *raiz;
+                *raiz = filho_escolhido;
+                free(musica_auxiliar);
+            }else{
+                Arvore_Musica *menor_direita;
+                menor_direita = buscar_menor_musica_direita((*raiz)->dir);
+                (*raiz)->dado = menor_direita->dado;
+                removeu = remover_musica(&(*raiz)->dir, menor_direita->dado.titulo);
+            }
+            removeu = 1;
+        } else {
+            if (strcmp(nome, (*raiz)->dado.titulo) < 0){
+                removeu = remover_musica(&(*raiz)->esq, nome);
+            }else if (strcmp(nome, (*raiz)->dado.titulo) > 0){
+                removeu = remover_musica(&(*raiz)->dir, nome);
+            }else{
+                removeu = 0;
+            }
+        }
+    }
+    return removeu;
+}
+
 
 Arvore_Musica *buscar_musica(Arvore_Musica *raiz, char nomeMusica[50]){
     Arvore_Musica *muscia;
